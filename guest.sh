c23972b1ea595d4b7940ab4a0dee72339abf8dcc
Nathan Peck <nathan@storydesk.com>
2015-04-12 13:08:00 -0400

    Adding nginx to the Vagrant to serve the static files and proxy the API

--- guest.sh
+++ guest.sh
@@ -2,10 +2,19 @@
 # dependencies / starts the Node.js servers
 
 sudo apt-get update
+sudo apt-get -y install git curl nginx
+
+# Configure nginx to server the client and server
+sudo rm -rf /etc/nginx/sites-available/*
+sudo rm -rf /etc/nginx/sites-enabled/*
+sudo cp /vagrant/conf/nginx/client /etc/nginx/sites-available/client
+sudo cp /vagrant/conf/nginx/server /etc/nginx/sites-available/server
+sudo ln -s /etc/nginx/sites-available/client /etc/nginx/sites-enabled/client
+sudo ln -s /etc/nginx/sites-available/server /etc/nginx/sites-enabled/server
+sudo service nginx reload
 
 # Trust Github
 ssh-keyscan github.com >> ~/.ssh/known_hosts
-sudo apt-get -y install git curl
 
 # Install n and use it install the version of nodejs we want
 if [ ! -d "/tmp/n" ]; then
@@ -15,7 +24,7 @@ cd /tmp/n; sudo make install
 sudo n 0.12.2
 
 # Install bower
-echo "Installing bower, may take a few moments...."
+echo "Installing bower via npm, may take a few moments...."
 sudo npm install -g bower
 
 # Install bower dependencies needed by the client
